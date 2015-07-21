ebc2e507916e85948e34a1f6da20d3990cb1d229
laxmikant <laxmikant@virtualemployee.com>
2015-07-21 11:40:04 +0530

    print msg2

--- app/src/main/java/com/example/laxmi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/laxmi/myapplication/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.print("Hello123456");
+        System.out.print("Virtual Employee");
     }
 
     @Override
