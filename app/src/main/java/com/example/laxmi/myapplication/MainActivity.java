4a7b74e15b84070847585a34a7ee8f56638094e8
laxmikant <laxmikant@virtualemployee.com>
2015-07-21 11:22:44 +0530

    print msg2

--- app/src/main/java/com/example/laxmi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/laxmi/myapplication/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.print("Hello123");
+        System.out.print("Hello123456");
     }
 
     @Override
